1ff7f2110340cfb737253655e98bd2e130dd78e4
David Gómez <38304163+DG-Develop@users.noreply.github.com>
2021-01-25 23:50:37 -0600

    Instalacion de redux thunk, configuracin de registro de usuario

---
+++ src/frontend/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+/* thunks son utilizados principalmente para retrasar la ejecución de alguna función hasta que su 
+resultado sea necesitado o para agregar una subrutina al principio o final de otra función. Esto 
+puede ser algo útil cuando tenemos una operación que puede ser muy costosa */
+import thunk from 'redux-thunk'
+import reducer from './reducers';
+import App from './routes/App';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const preloadedState = window.__PRELOADED_STATE__;
+const store = createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(thunk)));
+const history = createBrowserHistory();
+
+delete window.__PRELOADED_STATE__;
+
+ReactDOM.hydrate(
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
